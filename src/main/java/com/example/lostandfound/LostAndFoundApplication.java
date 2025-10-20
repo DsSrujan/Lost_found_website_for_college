70757e450c298344fd15533c2e434a33d4268d11
Srujan Ds <srujan2182005@gmail.com>
2025-10-20 17:42:21 +0530

    hope

--- src/main/java/com/example/lostandfound/LostAndFoundApplication.java
+++ src/main/java/com/example/lostandfound/LostAndFoundApplication.java
@@ -2,8 +2,10 @@ package com.example.lostandfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 
-@SpringBootApplication
+// Add the exclude attribute here
+@SpringBootApplication(exclude = {H2ConsoleAutoConfiguration.class})
 public class LostAndFoundApplication {
 
     public static void main(String[] args) {
